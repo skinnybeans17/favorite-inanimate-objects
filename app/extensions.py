913e4e3a8c03320b11d48aabc9accfb9924dea98
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-05-02 12:48:02 -0700

    fixed

--- app/extensions.py
+++ app/extensions.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from app.config import Config
+from app.models import User
 import os
 
 app = Flask(__name__)
@@ -16,5 +17,12 @@ db = SQLAlchemy(app)
 
 # TODO: Add authentication setup code here!
 
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
+bcrypt = Bcrypt(app)
