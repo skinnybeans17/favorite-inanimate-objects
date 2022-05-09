fc35bdd2bccb35cc2bdec45a04a3d5b46aec026c
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-05-09 12:39:24 -0700

    maybe this one

---
+++ app/auth/__init__.py
@@ -0,0 +1,18 @@
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+from app.models import User
+from app.extensions import app
+from app.auth.routes import auth
+
+app.register_blueprint(auth)
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+bcrypt = Bcrypt(app)
