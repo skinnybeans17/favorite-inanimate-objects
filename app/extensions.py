d47d7c1110651d1c0b9e93bc9742831a67e75ec5
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-05-09 16:24:12 -0700

    app

--- app/extensions.py
+++ app/extensions.py
@@ -12,7 +12,7 @@ app.config.from_object(Config)
 
 db=SQLAlchemy(app)
 
-login_manager = LoginManager()
+login_manager = LoginManager(app)
 login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
 
