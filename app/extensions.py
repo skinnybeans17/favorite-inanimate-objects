72c85d1e56fa7f1f0b859708a0b5eaa664140900
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-05-09 12:44:43 -0700

    now?

--- app/extensions.py
+++ app/extensions.py
@@ -3,10 +3,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from app.config import Config
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
 bcrypt = Bcrypt(app)
