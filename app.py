97ce8ff42bf65ea1c3ed2d4e56a5a80de6001813
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-05-09 12:29:11 -0700

    hopefully this works

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5002)
--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from app.main.routes import main
 app.register_blueprint(main)
 
 login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
+login_manager.login_view = 'main.homepage'
 login_manager.init_app(app)
 
 @login_manager.user_loader
